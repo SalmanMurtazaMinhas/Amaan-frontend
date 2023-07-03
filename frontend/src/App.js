28886b1974c9651b1fcb4267eb85d9559b15c537
zeemohamed7 <zainabshmohamed7@gmail.com>
2023-07-03 19:52:36 +0300

    create mood tracker

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,37 @@
-import React, { useState } from 'react';
-import 'react-calendar/dist/Calendar.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import JournalCreate from './components/journal/journalCreate';
+import JournalIndex from './components/journal/journalIndex';
 import MoodTracker from './components/MoodTracker';
-import '../src/App.css'
 
-function App() {
+export default function App() {
 
-  return (
-    <div>
-      <MoodTracker />
-    </div>
-  );
+    return(
+        <div>
+            <h1>React App</h1>
+            <Router>
+                <nav>
+                    <div>
+                    <Link to="/create-journal">Add a Journal</Link> &nbsp;
+                    <Link to="/journal">Journal</Link> &nbsp;
+                    <Link to="/mood">Mood</Link> &nbsp;
+                    </div>
+                </nav>
+                <Routes>
+                    <Route
+                    path='/create-journal'
+                    element={<JournalCreate />}
+                    />
+                    <Route
+                    path='/journal'
+                    element={<JournalIndex />}
+                    />
+                    <Route
+                    path='/mood'
+                    element={<MoodTracker />}
+                    />
+                </Routes>
+            </Router>
+        </div>
+    )
 }
-
-export default App
