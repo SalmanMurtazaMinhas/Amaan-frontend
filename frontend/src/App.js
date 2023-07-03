31fb5464fc2cd38ae887a07ac18475c26e75ab1b
Salman Murtaza <Salman.Murtaza.Minhas@outlook.com>
2023-07-03 12:13:39 +0300

    added displaying journal detail component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import JournalCreate from './components/journal/journalCreate';
 import JournalIndex from './components/journal/journalIndex';
+import JournalDetail from './components/journal/journalDetail';
 
 export default function App() {
+
+    
 
     return(
         <div>
@@ -11,7 +14,7 @@ export default function App() {
             <Router>
                 <nav>
                     <div>
-                    <Link to="/create-journal">Add a Journal</Link> &nbsp;
+                    {/* <Link to="/create-journal">Add a Journal</Link> &nbsp; */}
                     <Link to="/journal">Journal</Link> &nbsp;
                     </div>
                 </nav>
@@ -23,6 +26,10 @@ export default function App() {
                     <Route
                     path='/journal'
                     element={<JournalIndex />}
+                    />
+                    <Route
+                    path='/journal/detail/:journalId'
+                    element={<JournalDetail/>}
                     />
                 </Routes>
             </Router>
