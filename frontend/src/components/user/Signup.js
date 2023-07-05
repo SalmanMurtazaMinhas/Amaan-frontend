d4b0ced46940b8c048e03a7e9ddf281b7bfe4898
Rowan Al Qassab <Rowanalqassab@Rowans-MacBook-Pro.local>
2023-07-05 09:35:05 +0300

    Updated Signup and signin forms

--- frontend/src/components/user/Signup.js
+++ frontend/src/components/user/Signup.js
@@ -5,7 +5,6 @@ import TextField from '@material-ui/core/TextField';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import { Typography } from '@material-ui/core';
-
 import { Container, Form } from "react-bootstrap"
 
 const customStyles = makeStyles({
@@ -19,14 +18,20 @@ const customStyles = makeStyles({
         height: 430,
         display: 'flex',
         justifyContent: 'center',
-
     },
     container: {
         paddingLeft: 50,
         paddingRight: 50,
     },
     card2: {
-        width: '50%'
+        width: '50%',
+        height: 480,
+        paddingTop: 20,
+    }, 
+    header: {
+        color: '#a088d5',
+        textAlign: 'center',
+        fontSize: 35,
     }
 })
 
@@ -34,7 +39,7 @@ const StyledButton = withStyles({
     root: {
       background: '#BEAEE2',
       color: '#FFFFFF',
-      width: 150,
+      width: '100%',
       fontSize: 15,
       marginTop: 5,
       '&:hover' : {
@@ -71,7 +76,9 @@ export default function Signup(props) {
         <Card className={classes.card2}>
             <CardContent>
             <Container className={classes.container}>
-            <Typography variant="h4">
+            <Typography 
+            variant="h5"
+            className={classes.header}>
                 CREATE AN ACCOUNT
             </Typography>
 
@@ -79,38 +86,38 @@ export default function Signup(props) {
                 <TextField 
                 className={classes.field}
                 label="First Name" 
+                name="firstName"
                 onChange={changeHandler}
-                color="primary"
                 fullWidth
                 required
                 >
                 </TextField>
-            </form>
-            <form>
+            
                 <TextField
                 className={classes.field}
                 label="Last Name"
+                name="lastName"
                 onChange={changeHandler}
                 fullWidth
                 required
                 >
                 </TextField>
-            </form>
-            <form>
-            <TextField
+            
+                <TextField
                 className={classes.field}
                 label="Email" 
+                name="emailAddress"
                 onChange={changeHandler}
                 fullWidth
                 required
                 >
                 </TextField>
-            </form>
-            <form>
+            
                 <TextField
                 className={classes.field}
                 label="Password"
                 type="password"
+                name="password"
                 onChange={changeHandler}
                 fullWidth
                 required
