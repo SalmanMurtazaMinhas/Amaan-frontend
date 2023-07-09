da463a8cdf937c22df581b81db0debb47456c896
Rowan Al Qassab <Rowanalqassab@Rowans-MacBook-Pro.local>
2023-07-09 09:06:22 +0300

    testing for changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -275,7 +275,7 @@ export default function App() {
                 </Routes>
 
             
-            <Footer />
+            {/* <Footer /> */}
              </div>
         </ThemeProvider>
     )
